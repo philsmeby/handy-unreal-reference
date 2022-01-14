e1a6be59528b1691689ad840fd5af2ec6527004e
Phil Smeby <phillip.smeby@hillrom.com>
2022-01-14 00:56:08 -0500

    change gitignore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// Site Configuration
-	lang: 'en-US',
 	title: 'Handy Unreal Engine Reference Guide',
 	description: 'A quick reference guide for the Unreal Engine 4.  Short and to the point!',
 	base: '/handy-unreal-reference/',
+	lang: 'en-US',
 
 	// theme and its config
 	theme: '@vuepress/theme-default',
